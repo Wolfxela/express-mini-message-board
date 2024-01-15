16a6fdc41d976b6b71e91145734338ab5810b4c3
Wolfxela <alexthegreitsas@gmail.com>
2024-01-15 04:16:30 +0200

    finished the mini board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const messages = [
+  {
+    content: "hello there!",
+    user: "sam",
+    date: new Date()
+  },
+  {
+    content: "ama fof?",
+    user: "bobby",
+    date: new Date()
+  }
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini message board', messages: messages });
 });
+
+router.get("/new",function(req,res,next ){
+  res.render('form')
+})
+router.post('/new',function(req,res,next){
+  messages.push({content: req.body.message_input, user: req.body.user_input, date: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
